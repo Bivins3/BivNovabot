e97b0a2101bffa59172d467ae8e2d24292551b57
Bivins3 <bivinsmack71@gmail.com>
2024-12-23 23:58:21 +0100

    Update bot_alertes.py

--- bot_alertes.py
+++ bot_alertes.py
@@ -1,9 +1,7 @@
 import time
-from datetime import datetime
 
 print("Le bot est en cours d'exécution...")
 
 while True:
-    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    print(f"🔔 Alerte : Nouvelle notification en temps réel à {current_time} !")
-    time.sleep(3 * 60 * 60)  # Pause de 3 heures
+    print("🔔 Alerte : Nouvelle notification en temps réel !")
+    time.sleep(10)  # Pause de 10 secondes entre chaque alerte
